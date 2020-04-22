7d2fff1428283f5ce48b5ec189363ca3b770edd5
Guillaume Huysmans <ghuysmans99@gmail.com>
2020-04-22 02:51:51 +0200

    Store dialogs in a Hashtbl

--- cli/demo.ml
+++ cli/demo.ml
@@ -6,27 +6,22 @@ let rec test () =
   send ("Hi " ^ u ^ "!") >>= fun () ->
   send "What's your favorite number?" >>= ask >>= function
     | Ping -> send "Not now, please..." >>= fun () -> test ()
-    | Message "42" -> send "I knew it!" >>= fun () -> return true
-    | Message _ -> send "Meh..." >>= fun () -> return false
+    | Message "42" -> send "I knew it!"
+    | Message _ -> send "Meh..."
 
 let () =
-  let a = ref @@ Interpreter.step @@ test () in
-  let b = ref @@ Interpreter.step @@ test () in
   try
     while true do
       let m = read_line () in
-      let u, target, m =
+      let u, m =
         if m.[0] = '&' then
-          "Bob", b, String.(sub m 1 (length m - 1))
+          "Bob", String.(sub m 1 (length m - 1))
         else
-          "Alice", a, m
+          "Alice", m
       in
-      match !target with
-      | Return r -> Printf.printf "returned %b, ignoring the input\n" r
-      | Ask f ->
-        target := Lexer.tokenize m |> Parser.parse |> f |> Interpreter.step
-      | User f -> target := f u |> Interpreter.step
-      | _ -> Printf.printf "stuck\n"
+      match Interpreter.update test u m with
+      | Waiting -> ()
+      | Done -> Printf.printf "done.\n"
     done
   with End_of_file ->
     ()
--- cli/interpreter.ml
+++ cli/interpreter.ml
@@ -1,5 +1,28 @@
 open Bot
 
-let rec step = function
-  | Dialog.Send (s, f) -> print_endline s; step (f ())
+type state =
+  | Done
+  | Waiting
+
+let dialogs : (string, unit Dialog.t) Hashtbl.t = Hashtbl.create 10
+
+let rec step user = function
+  | Dialog.Send (s, f) -> print_endline s; step user (f ())
+  | User f -> step user (f user)
   | stuck -> stuck
+
+let update init user message =
+  let d =
+    match Hashtbl.find_opt dialogs user with
+    | None -> step user (init ())
+    | Some d -> d
+  in
+  match d with
+  | Return () ->
+    Hashtbl.remove dialogs user;
+    Done
+  | Ask f ->
+    Lexer.tokenize message |> Parser.parse |> f |> step user |>
+    Hashtbl.replace dialogs user;
+    Waiting
+  | _ -> failwith "stuck" (* FIXME? *)
